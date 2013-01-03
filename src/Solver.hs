f840f167117f5604c8ffbd0b50f9394bb982d0c7
Joshua Ball <JoshBBall@gmail.com>
2013-01-02 20:57:01 -0800

    Removed commented code

--- src/Solver.hs
+++ src/Solver.hs
@@ -259,47 +259,6 @@ pop set  = do
   set .= s'
   return (Just v)
 
-{-
-backtrack :: Solve c ()
-backtrack = do
-  mbVar <- selectUnassignedVariable
-  case mbVar of
-    Nothing -> escape
-    Just var -> do
-      everyValue var $ \newvars newclauses -> do
-        appendVars newvars
-        appendClauses newclauses
-        (continue, undo) <- reviseAll 
-        when continue backtrack
-        undo
-      -- todo: stick clause learning somewhere in here
-      -- The argument to runNew should be (return True), since all
-      -- learnt clauses can be garbage collected.
-      liftIO $ unset var
-
-selectUnassignedVariable :: Solve c (Maybe (UntypedIvar c))
-selectUnassignedVariable = do
-  vs <- vars
-  sizes <- mapM (\v -> (,v) <$> liftIO (numCandidates v)) . S.toList $ vs
-  case sort . filter (\(x,_) -> x > 1) $ sizes of
-    [] -> return Nothing
-    ((_,v):_) -> return (Just v)
-
-reviseAll :: Solve c (Bool, Solve c ())
-reviseAll = undefined {- do
-  cs <- clauses
-  consume (return ()) cs where
-    consume undo [] = return (True, undo)
-    consume undo (x:xs) = do
-      (continue, affectedVars, undo') <- runAssign $ revise x -- should run side effects
-      -- todo: need to add clauses associated with affectedVars back into queue
-      if continue then do
-        sideEffects
-        consume (undo >> undo') xs
-      else return (False, undo)
-      -}
--}
-
 newVar = Var <$> newUnique <*> newIORef S.empty
 
 -- | Creates an abstract variable.
@@ -445,39 +404,3 @@ learn :: [c] -> Solve c (New c ())
 learn xs = Solve $ do
   learner <- ask
   return (learner xs)
-
-----
-
-{-
-propagate vs p clauses = do
-  (newConstraints, implications) <- foldThing genImplications clauses
-  case implications of
-    Nothing -> return False
-    Just xs -> do
-      cs <- newConstraints
-      if (not . null $ cs)
-        then propagate (map Left xs ++ vs) p cs
-        else assign (map Left xs ++ vs) p
-
--- ergh... propagating constraints is not enough; if a constraint just
--- slightly limits the acceptable values for a variable, then we must
--- iterate over the acceptable values
-assign :: [Either (IO [Clause], IO ()) RequiredVar] -> Problem clause a -> IO Bool
-assign [] _ = return True
-assign (Right v : vs) p = do -- non-deterministic assignment
-  set <- assigned v
-  if set then assign vs p else do
-  ret <- anyValue v $ do
-    clauses1 <- creations v
-    clauses2 <- constraining v
-    propagate vs p (clauses1 ++ clauses2)
-  if ret then return True else do
-  unassign v
-  return False
-assign (Left (set, unset) : vs) p = do -- deterministic assignment
-  clauses <- set -- should give both the creations and the constraints
-  ret <- propagate vs p clauses
-  if ret then return True else do
-  unset
-  return False
-  -}
