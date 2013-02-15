4dd8d649f79348ec579bf108b3f9ba6a0ced8a48
Joshua Ball <JoshBBall@gmail.com>
2013-02-15 14:09:46 -0800

    started implementing dynamic solve

--- scala/src/main/scala/com/sciolizer/taffy/DynamicSolver.scala
+++ scala/src/main/scala/com/sciolizer/taffy/DynamicSolver.scala
@@ -1,6 +1,6 @@
 package com.sciolizer.taffy
 
-import util.control.Breaks._
+import collection.mutable
 
 /**
  * Created with IntelliJ IDEA.
@@ -8,13 +8,43 @@ import util.control.Breaks._
  * Date: 2/15/13
  * Time: 11:32 AM
  */
-class DynamicSolver[Constraint, Values, Value](domain: Domain[Constraint, Values, Value], ranger: Ranger[Values, Value]) {
+class DynamicSolver[Constraint, Values, Value](domain: Domain[Constraint, Values, Value], ranger: Ranger[Values, Value], candidateValues: Values) {
   def solve[A, B](problem: Init[Constraint, Value] => A, solutionReader: (A, Reader[Value]) => B): Option[B] = {
+    val constraints: mutable.Set[Constraint] = mutable.Set.empty
+    var varCounter = 0
+    val init = new Init[Constraint, Value] {
+      def newVariable(sideEffects: SideEffects[Constraint, Value]): Variable[Value] = {
+        val ret: Variable[Value] = new Variable(varCounter, {
+          throw new NotImplementedError()
+//          sideEffects.run(ret, hm, init)
+        })
+        varCounter += 1
+        ret
+      }
 
+      def newConstraint(constraint: Constraint) {
+        constraints += constraint
+      }
+
+      def newTemplate[A](template: (Instantiator[Constraint, Value]) => A): Template[Constraint, Value, A] = throw new NotImplementedError()
+    }
+
+    val handle: A = problem(init)
+
+    val p = new Problem[Constraint, Values, Value](varCounter, Set.empty ++ constraints, candidateValues, NoIsomorphisms) // todo: allow isomorphisms
+    val sss = new SuperSimpleSolver[Constraint, Values, Value](domain, p, ranger)
+    sss.backtrackingSearch((0 until varCounter).map(x => x -> candidateValues).toMap) match {
+      case None => throw new NotImplementedError() // need to run side effects
+      case Some(solution) =>
+        val reader = new Reader[Value] {
+          def read(vid: Int): Value = solution(vid)
+        }
+        Some(solutionReader(handle, reader))
+    }
   }
 }
 
-class Variable[Value](val varId: Int)
+class Variable[Value](val varId: Int, effects: => Unit)
 
 trait Instantiator[Constraint, Value] {
   def newVariable(sideEffects: SideEffects[Constraint, Value] = noSideEffects): Variable[Value]
